a7613b9604f60d4e6ec24115856f43c3ae5209b1
ilPesce <cole.hill41@gmial.com>
2020-02-09 16:40:11 -0500

    Added comments

--- track.py
+++ track.py
@@ -38,7 +38,12 @@ def video_to_frames(video,directory):
         im.imwrite(os.path.join(directory,'frame{}.png'.format(i)),data[i])
 
 @numba.autojit
-def get_eigs(img):
+def get_corners(img):
+    """
+    Finds all of the good feature points in the image
+    """
+
+    #Controls the size of the the gaussian kernels
     div_window = 10
     window_size = 20
 
@@ -50,6 +55,8 @@ def get_eigs(img):
     a = gaussian_filter(imgx,1,truncate=window_size)
     b = gaussian_filter(imgxy,1,truncate=window_size)
     c = gaussian_filter(imgy,1,truncate=window_size)
+
+    #Iterates through each pixel and gets an interest value
     eigs = np.ndarray(shape=img.shape)
     for i in numba.prange(img.shape[0]):
         for j in numba.prange(img.shape[1]):
@@ -59,34 +66,46 @@ def get_eigs(img):
     return eigs
 
 def get_displacement(I,J):
+    """
+    Determines delta for newton-raphson optimization
+    """
     I = I/255
     J = J/255
     #Gradient window
     grad_window = 5
     c = int(grad_window/2)
+
     #Get gradient at center of window
     gx = gaussian_filter1d(J,1,axis=0,order=1,truncate=grad_window)
     gy = gaussian_filter1d(J,1,axis=1,order=1,truncate=grad_window)
-    # gx = scipy.ndimage.sobel(J,axis=1)
-    # gy = scipy.ndimage.sobel(J,axis=0)
 
     e1 = np.sum((J-I)*gx)
     e2 = np.sum((J-I)*gy)
     e = -np.array([e1,e2]).T
+
     gx=np.sum(gx)
     gy=np.sum(gy)
+
+    #Find delta assuming just translation
     Z = np.array([[gx**2,gx*gy],[gx*gy,gy**2]])
 
     d = np.linalg.pinv(Z)@e
     return d
 
 def get_feature_points(img):    
+    """
+    Finds set of feature points in an image
+    """
     
-    eigs = get_eigs(img)
+    #Get all points w/ value indicating uniquness
+    eigs = get_corners(img)
+
+    #Just keep the 50 with highest uniqueness
     vals = eigs.flatten()
     vals.sort()
-    ratio = .005
     t = vals[-50:]
+    
+    #Organize points into a list
     mask = np.where(eigs>t[0],1,0)
     plist = []
     for i in range(mask.shape[0]):
@@ -97,50 +116,66 @@ def get_feature_points(img):
 
 @numba.autojit
 def track_points(imgs,_imgs,pnts):
+    """
+    Track points from first grayscale image and add red dots over those
+    points in _imgs a colored image array
+    """
+    
+    #Starting image
     img = imgs[0]
+    
     for i in range(1,len(imgs)):
-        print(i)
+        
+        #Array for new points
         npnts = []
-        # print(pnts)
+        #window size for searching for point delta
         win_size = 5
         c = int(win_size/2)
+        
         for pnt in pnts:
-            # print(pnt)
+            #Pixel value
             x,y = int(pnt[0]),int(pnt[1])
+            
+            #Add markers in first image
             if i ==1:
                 _imgs[0][x,y] = np.array([255,0,0])
+            #Check if pixel value is invalid
             if x<c or x>img.shape[0]-(c+1):
                 continue
             if y<c or y>img.shape[1]-(c+1):
                 continue
+            
+            #Determine new point
             dt = 100
+            #Point window in first image
             window_1 = imgs[i-1][x-c:x+c,y-c:y+c]
             cnt = 0
-            min_err = 1e6
-            min_xy = (x,y)
+            #Continue iteration until stop conditions are met
             while dt>0.01 and cnt<200:
+                #Increment iteration count
                 cnt +=1
+                #Pixel value
                 xt,yt =int(x),int(y)
+                #Get window in next frame
                 window_2 = imgs[i][xt-c:xt+c,yt-c:yt+c]
-                err = np.sum(np.abs(window_1-window_2))
-                if err<min_err:
-                    min_err = err
-                min_xy = (xt,yt)
+                #Determine translation value
                 d = get_displacement(window_1,window_2)
-                # d0 = math.ceil(np.abs(d[0]))*d[0]/np.abs(d[0]+0.0001)
-                # d1 = math.ceil(np.abs(d[1]))*d[1]/np.abs(d[1]+0.0001)
                 d0 = d[0]
                 d1 = d[1]
                 x,y = x+d0,y+d1
+                #Measure movement
                 dt = np.abs(d[0]) + np.abs(d[1])
+                
+                #Pixel has moved off screen
                 if x<c or x>img.shape[0]-(c+1):
                     break
                 if y<c or y>img.shape[1]-(c+1):
                     break
-                # print(pnt,dt,(x,y))
-            print(dt)
+            #Set new pixel values for point
             xi,yi = int(x),int(y)
             npnts.append((xi,yi))
+            
+            #Add markers for points in color image
             for pnt in npnts:
                 x,y = map(int,pnt)
                 if x<0 or x>_imgs[i].shape[0]-1:
@@ -153,17 +188,23 @@ def track_points(imgs,_imgs,pnts):
 
 
 def points_from_frames(frame_dir,outdir = None):
+    """
+    Track points from frames in a directory
+    """
+    
+    #Get list of frame files
     join = os.path.join
     frame_flist = os.listdir(frame_dir)
     
+    #Filter for just png's and put them in numerical order
     temp = []
     for f in frame_flist:
         if f.lower().endswith('.png'):
             temp.append(f)
     frame_flist = temp
-    frame_flist = sorted(frame_flist,key=lambda x: int(x.split('.')[0]))[:10]
-    print(frame_flist)
-
+    frame_flist = sorted(frame_flist,key=lambda x: int(x.split('.')[0]))
+    
+    #Import the image data and split into color and grayscale images
     frames = [im.imread(join(frame_dir,x)) for x in frame_flist]
     if len(frames[0].shape)>2:
         gframes = [x[:,:,0] for x in frames]
@@ -178,9 +219,12 @@ def points_from_frames(frame_dir,outdir = None):
             _frames.append(temp)
         frames = _frames
 
+    #Get points to track in first image
     pnts = get_feature_points(gframes[0])
+    #Track points in all other frames
     pnts,frames = track_points(gframes,frames,pnts)
     
+    #Output new frames
     if outdir is None:
         outdir = join(frame_dir,"output")
         os.makedirs(outdir,exist_ok=True)
